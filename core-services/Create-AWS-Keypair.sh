45d9903324230610f032fc77b71ac2853f87f337
Brian Yoo <157921713+rhit-Yoob@users.noreply.github.com>
2026-01-24 22:56:18 -0500

    Add comment for SSH key pair creation
    Added a comment explaining the purpose of the key pair creation.

--- core-services/Create-AWS-Keypair.sh
+++ core-services/Create-AWS-Keypair.sh
@@ -1,1 +1,2 @@
-aws ec2 create-key-pair --key-name "devopKey" --key-type "rsa"
+# Creating an SSH key pair named devopkey that lets you securely connect to your EC2 instances
+aws ec2 create-key-pair \ --key-name "devopKey" \ --key-type "rsa"
