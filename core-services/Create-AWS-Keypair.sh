62e7a3c561dafafb867a237a51db26fc51b47780
Brian Yoo <157921713+rhit-Yoob@users.noreply.github.com>
2026-01-26 00:10:43 -0500

    Fix formatting of create-key-pair command

--- core-services/Create-AWS-Keypair.sh
+++ core-services/Create-AWS-Keypair.sh
@@ -1,2 +1,2 @@
 # Creating an SSH key pair named devopkey that lets you securely connect to your EC2 instances
-aws ec2 create-key-pair \ --key-name "devopKey" \ --key-type "rsa"
+aws ec2 create-key-pair --key-name "devopKey" --key-type "rsa"
